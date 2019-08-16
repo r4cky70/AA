c8299dcf5a027e2e25058be3677eadf3e0216549
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-08-16 00:39:06 -0300

    random

--- 2019.2/Intermediario I/lista2/a.cpp
+++ 2019.2/Intermediario I/lista2/a.cpp
@@ -6,110 +6,58 @@
 #define pb push_back
 #define INP freopen("input.txt", "r", stdin)
 #define OUT freopen("out.txt", "w", stdout)
+#define STREE int bch = no*2, mid = (l+r)/2
+#define all(a) a.begin(), a.end()
 
 using namespace std;
 typedef long long ll;
 typedef vector < int > vi;
 typedef pair < int, int > pii;
-typedef vector < pii > vpii;
+typedef vector < pair<ll, ll > > vpii;
+
 
 int n, q;
-pii arr[MAXS];
- 
+pii so[MAXS];
+int arr[MAXS];
 
-bool func(pii a, pii b){
-  return a.second < b.second;
-}
+struct segtree{
+  vi tree[MAXS*4];
 
-vpii merge(vpii a, vpii b){
-  vpii temp;
-  int p1 = 0, p2 = 0;
-  while(p1 != a.size() || p2 != b.size()){
-    if(p1 == a.size()){
-      temp.push_back(b[p2]);
-      ++p2;    
-    }else if(p2 == b.size()){
-      temp.push_back(a[p1]);
-      ++p1;
-    }
-    else if(a[p1].first < b[p2].first){
-      temp.pb(a[p1]);
-      ++p1;
-    }else{
-      temp.pb(b[p2]);
-      ++p2;
-    }
-  }
-  return temp;
-}
-
-int mylower(vpii a, int l, int r, int v){
-  int mid = (l+r)/2;
-  if(l == r) return l;
-  else if(a[mid].first >= v){
-    return mylower(a, l, mid, v);
-  }else{
-    return mylower(a, mid+1, r, v);
-  }
-}
-int myupper(vpii a, int l,int r, int v){
-  int mid = (l+r)/2;
-  if(l == r) return l;
-  else if(a[mid].first <= v){
-    return myupper(a, mid+1, r, v);
-  }else{
-    return myupper(a, l, mid, v);
-  }
-}
-bool funci(pii a, pii b){
-  return (a.first < b.first);
-}
-
-
-struct mergetree{
-  vpii tree[MAXS];
-
-  int busca(int no, int l, int r, int x, int y){
-    return lower_bound(tree[no].begin(), tree[no].end(), x, funci);
-    return (l+r+1) - mylower(tree[no], l, r, x) - myupper(tree[no], l, r, y);
-  }
   void build(int no, int l, int r){
-    if(l == r){
-      tree[no].push_back(arr[l]);
+    if(l==r){
+      tree[no].pb(so[l].second);
     }else{
       int mid = (l+r)/2;
       build(no*2, l, mid);
       build(no*2+1, mid+1, r);
-      tree[no] = merge(tree[no*2], tree[no*2+1]);
+      merge(tree[no*2].begin(), tree[no*2].end(), tree[no*2+1].begin(), tree[no*2+1].end(), back_inserter(tree[no]));
     }
   }
   int query(int no, int l, int r, int x, int y, int v){
-    if(l == r) return tree[no][0].second;
+    if(l == r) return arr[tree[no][0]];
+    int a = upper_bound(tree[no*2].begin(), tree[no*2].end(), y) - lower_bound(tree[no*2].begin(), tree[no*2].end(), x);
     int mid = (l+r)/2;
-    int a = busca(no*2, l, mid, x, y);
-    int b = busca(no*2+1, mid+1, r, x, y);
-    if(v > a) return query(no*2+1, mid+1, r, x, y, v-a);
-    else return query(no*2, l, mid, x, y, v);
+    if(a >= v){
+      return query(no*2, l, mid, x, y, v);
+    }else return query(no*2+1, mid+1, r, x, y, v-a);
   }
-
 }root;
 
+
+
 int main(){
-  // INP;OUT;
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  
-  int temp;
   cin >> n >> q;
   for(int i = 0; i < n; ++i){
-    cin >> temp;
-    arr[i] = {i, temp};
+    cin >> arr[i];
+    so[i] = {arr[i], i};
   }
-  sort(arr, arr+n, func);
+  sort(so, so+n);
   root.build(1, 0, n-1);
   int a, b, c;
-  for(int i = 0 ; i < q; ++i){
+  for(int i = 0; i < q; ++i){
     cin >> a >> b >> c;
     cout << root.query(1, 0, n-1, a-1, b-1, c) << "\n";
   }
 }
+
+
--- 2019.2/Intermediario I/lista2/teste.cpp
+++ 2019.2/Intermediario I/lista2/teste.cpp
@@ -1,47 +1,32 @@
 #include <bits/stdc++.h>
 #define MAXS 100007
-#define INF 2234567890
+#define INF 2147483647
 
 #define make_pair(a, b) mp(a,b)
-#define pb push_back
+#define pb(a) push_back(a)
 #define INP freopen("input.txt", "r", stdin)
 #define OUT freopen("out.txt", "w", stdout)
+#define STREE int bch = no*2, mid = (l+r)/2
+#define ALL(a) a.begin(), a.end()
 
 using namespace std;
 typedef long long ll;
 typedef vector < int > vi;
 typedef pair < int, int > pii;
-typedef vector < pii > vpii;
-
-int mylower(vi a, int l, int r, int v){
-  int mid = (l+r)/2;
-  if(l == r) return l;
-  else if(a[mid] >= v){
-    return mylower(a, l, mid, v);
-  }else{
-    return mylower(a, mid+1, r, v);
-  }
-}
-int myupper(vi a, int l,int r, int v){
-  int mid = (l+r)/2;
-  if(l == r) return l;
-  else if(a[mid] <= v){
-    return myupper(a, mid+1, r, v);
-  }else{
-    return myupper(a, l, mid, v);
-  }
-}
-
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 
 int main(){
-  vi a;
-  for(int i = 0; i < 8; ++i){
-    a.pb(i*2);
+  vi idk(10);
+  for(int i = 0; i < 10; ++i){
+    idk[i] = i;
   }
-  for(int i = 0; i < 8; ++i){
-    cout << a[i] << " ";
-  }cout << endl;
-  cout << mylower(a, 0, a.size()-1, 2) << endl;
-  cout << myupper(a, 0, a.size()-1, 2) << endl;
+  shuffle(idk.begin(), idk.end(), rng);
+  uniform_int_distribution<ll> t;
+  cout << t << endl << endl;
+  cout << INF << endl;
+  for(int i =0 ;i < 10; ++i){
+    cout << idk[i] << endl;
+  }
+  
 }
