592d646f005dd9672a64e9a279869518b77be236
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-07 11:55:34 -0300

    builtin

--- 2019.2/Intermediario I/lista5/teste.cc
+++ 2019.2/Intermediario I/lista5/teste.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define MAXS 100007
-#define INF 2147483647
+#define INF (int)__builtin_inf()
 
 #define mp(a, b) make_pair(a,b)
 #define pb(a) push_back(a)
@@ -32,9 +32,14 @@ void teste(vector<int> v, int a){
 int main(){
   string s = "hello world";
   vector<int> yo{1, 2, 3, 4, 5};
-  teste(yo, 2);
-  auto f = [&yo, a = 2]{for(int i = 0; i < yo.size(); i+=a) cout<<yo[i]<<endl;};
-  for(int i = 0; i < 3; ++i){
-    f();
-  }
+  // teste(yo, 2);
+  // auto f = [&yo, a = 2]{for(int i = 0; i < yo.size(); i+=a) cout<<yo[i]<<endl;};
+  // for(int i = 0; i < 3; ++i){
+  //   f();
+  // }
+  // int a = 64;
+  // cout << (1<<__builtin_ctz(a)) << endl << (a&-a) << endl << __builtin_clz(1) - __builtin_clz(a) << endl;
+  
+  cout << (int)__builtin_inf() << endl << (ll)__builtin_inf() << endl << (int)(unsigned char)__builtin_inf() << endl;
+  cout << INF << endl;
 }
