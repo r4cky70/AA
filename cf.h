edb5888f8c3a4a817a00ba9f7c036a2ba97afe5e
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-08 18:02:14 -0300

    updating my new and shynning template

--- cf.h
+++ cf.h
@@ -1,13 +1,8 @@
 #include <bits/stdc++.h>
-#define INF (int)__builtin_inf()
-#define MAXS 100007
 
 #define mp(a, b) make_pair(a,b)
 #define pb(a) push_back(a)
-#define INP freopen("input.txt", "r", stdin)
-#define OUT freopen("out.txt", "w", stdout)
 #define all(a) a.begin(), a.end()
-#define FASTIO ios::sync_with_stdio(false); cin.tie(0)
 #define debug(...) cerr<<"["<<#__VA_ARGS__<<": "<<(__VA_ARGS__)<<"]\n"
 #define debugarr(a, b) for(int i=0;i<b;++i)\
 cerr<<a[i]<<" "; cerr<<endl
@@ -18,7 +13,24 @@ typedef vector < int > vi;
 typedef pair < int, int > pii;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+const int INF = (int)__builtin_inf();
+const int MAXS = 100007;
+
+void solve(){
+
+  
+
+}
 
 int main(){
-  
+  cout.precision(12);
+  cout << fixed;
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  solve();
+  cerr << "\nTime: " << 1.0*clock()/CLOCKS_PER_SEC << " s.\n";
+#else
+  ios::sync_with_stdio(false);cin.tie(0);
+  solve();
+#endif
 }
